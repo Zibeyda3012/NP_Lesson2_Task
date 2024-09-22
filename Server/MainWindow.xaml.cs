92cfe952c0edfb9af274dc2a1ba45366fe73cf4d
Zibeyda3012 <musayevazibeyd@gmail.com>
2024-09-22 19:46:50 +0400

    Add project files.

---
+++ Server/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace Server;
+
+public partial class MainWindow : Window, INotifyPropertyChanged
+{
+    private int port;
+
+    public int Port { get => port; set { port = value; OnPropertyChanged(); } }
+
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        DataContext = this;
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string name = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
